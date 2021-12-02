2c3bf9cec46ba34735ea461886b16a6fae545cf3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2021-12-02 11:25:05 +0700

    Implement the services

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # simple-azure-ad-deamon
 Azure AD deamon for my apps
+
+## Workflow
+- User enter this service ('/')
+- This service redirect to MS login page
+- MS redirect to this service ('/redirect') to login info
+- This service use the login info to mint new token
+- Response the token
+
+## How to use
+
+- Clone this
+- `npm i`
+- Copy `.env.example` and rename it to `.env`
+- Set configs in `.env`
+- `npm run start`
+
+
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const msal = require('@azure/msal-node');
+const admin = require("firebase-admin");
+require('dotenv').config();
+
+const SERVER_PORT = process.env.PORT || 3000;
+const REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:3000/redirect";
+const CLIENT_ID = process.env.CLIENT_ID;
+const TENANT_ID = process.env.TENANT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const FIREBASE_SERVICE_ACCOUNT_PATH = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+
+const app = express();
+const config = {
+    "auth": {
+        "clientId": CLIENT_ID,
+        "authority": "https://login.microsoftonline.com/" + TENANT_ID,
+        "clientSecret": CLIENT_SECRET
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
+};
+const cca = new msal.ConfidentialClientApplication(config);
+
+const serviceAccount = require(FIREBASE_SERVICE_ACCOUNT_PATH);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: FIREBASE_DATABASE_URL,
+});
+
+app.get('/', (req, res) => {
+    const authCodeUrlParameters = {
+        scopes: ["user.read"],
+        redirectUri: REDIRECT_URI,
+    };
+    cca.getAuthCodeUrl(authCodeUrlParameters)
+        .then((ccaRes) => {
+            res.redirect(ccaRes);
+        }).catch((error) => {
+            console.error(error);
+            res.status(500).send(error);
+        });
+});
+
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        scopes: ["user.read"],
+        redirectUri: REDIRECT_URI,
+    };
+    cca.acquireTokenByCode(tokenRequest)
+        .then((ccaRes) => {
+            admin.auth().createCustomToken(ccaRes.account.localAccountId)
+                .then(function (token) {
+                    res.status(200).send({ token: token });
+                }).catch((error) => {
+                    console.error(error);
+                    res.status(500).send(error);
+                });
+        }).catch((error) => {
+            console.error(error);
+            res.status(500).send(error);
+        });
+});
+
+app.listen(SERVER_PORT, () => console.log(`Azure AD Deamon is listening on port ${SERVER_PORT}`));
