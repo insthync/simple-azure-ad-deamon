d8c2c86973f805c82ade1128c4dee14f97059449
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2021-12-08 11:46:44 +0700

    Implements https

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const msal = require('@azure/msal-node');
 const admin = require("firebase-admin");
+const fs = require('fs');
+const https = require('https');
 require('dotenv').config();
 
 const SERVER_PORT = process.env.PORT || 3000;
@@ -10,7 +12,12 @@ const TENANT_ID = process.env.TENANT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const FIREBASE_SERVICE_ACCOUNT_PATH = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
 const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH;
+const SSL_CRT_PATH = process.env.SSL_CRT_PATH;
 
+var privateKey  = fs.readFileSync(SSL_KEY_PATH, 'utf8');
+var certificate = fs.readFileSync(SSL_CRT_PATH, 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 const app = express();
 app.set ( "view engine", "ejs" );
 
@@ -74,4 +81,5 @@ app.get('/redirect', (req, res) => {
         });
 });
 
-app.listen(SERVER_PORT, () => console.log(`Azure AD Deamon is listening on port ${SERVER_PORT}`));
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(SERVER_PORT, () => console.log(`Azure AD Deamon is listening on port ${SERVER_PORT}`));
